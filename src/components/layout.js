3b78fcea886bf2e0fdd011587609676dee999847
Zeeshan Junaid <zeeshanjunaid2222@gmail.com>
2020-09-05 05:47:12 +0500

    final commit of the day

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,12 +8,30 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Header from "./header"
+import Helmet from "react-helmet"
+import { useStaticQuery, graphql } from "gatsby"
 import "../styles/main.scss"
 import Footer from "./footer"
 
 const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      headerContent: contentfulHeader(
+        id: { eq: "bd4009d2-c1ff-5e7f-83ee-512d12284d3c" }
+      ) {
+        favicon {
+          fluid {
+            src
+          }
+        }
+      }
+    }
+  `)
   return (
     <>
+      <Helmet>
+        <link rel="icon" href={data.headerContent.favicon.fluid.src} />
+      </Helmet>
       <Header />
       <main>{children}</main>
       <Footer />
