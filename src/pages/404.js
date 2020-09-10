02b19574199d3c1b4f3b7cbe882e434a951c9894
Zeeshan Junaid <zeeshanjunaid2222@gmail.com>
2020-09-10 14:38:46 +0500

    added 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,43 @@
 import React from "react"
-
+import { useStaticQuery, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Container, Row, Col } from "react-bootstrap"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      contentful404Page(id: { eq: "97d673b9-870b-5c03-80e3-5d0f7c918329" }) {
+        heading
+        description
+        backgroundImage {
+          file {
+            url
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <Layout>
+      <SEO title="404: Not found" />
+      <div
+        class="notfound"
+        style={{
+          backgroundImage: `url(${data.contentful404Page.backgroundImage.file.url})`,
+        }}
+      >
+        <Container>
+          <Row className="align-items-center">
+            <Col md sm={12} className="hero__text-container">
+              <h2>{data.contentful404Page.heading}</h2>
+              <p>{data.contentful404Page.description}</p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </Layout>
+  )
+}
 
 export default NotFoundPage
